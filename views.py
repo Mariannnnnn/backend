c351e6dbfbbeef1d3416d0f5bc4d5bb62c7c7c06
Mariannnnnn <94128368+Mariannnnnn@users.noreply.github.com>
2021-12-09 10:55:35 +0000

    Add files via upload

---
+++ views.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, render_template, redirect,url_for
+
+views = Blueprint(__name__, "views")
+
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+@views.route("")
+def page_one():
+    return render_template("")
+
+@views.route("")
+def page_two():
+    return render_template("")
+
+@views.route("")
+def page_three():
+    return render_template("")
+
+@views.route("")
+def page_four():
+    return render_template("")
+
+@views.route("")
+def page_five():
+    return render_template("")
+
+@views.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@views.route("/javascript")
+def javascript_redirect():
+    return redirect(url_for("views.home"))
+
+@views.route("/js")
+def js_redirect():
+    return redirect(url_for("views.home"))
+
+@views.route("/home")
+def home_redirect():
+    return redirect(url_for("views.home"))
